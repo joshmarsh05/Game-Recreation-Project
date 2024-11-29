123c12aca717fd315fdad58c53785f800112b2f7
joshmarsh05 <joshmarsh05@gmail.com>
2024-11-28 19:16:00 -0500

    Title screen done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
